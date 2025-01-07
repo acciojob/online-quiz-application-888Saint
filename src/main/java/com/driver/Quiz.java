8272a21861e1c4a1fe1596d6b55fec2ec85e37a7
Saint <psdas32@gmail.com>
2025-01-07 12:31:57 +0530

    final commit

--- src/main/java/com/driver/Quiz.java
+++ src/main/java/com/driver/Quiz.java
@@ -7,14 +7,19 @@ public class Quiz {
 
     public Quiz() {
         // your code goes here;
+        this.questions = new ArrayList<>();
     }
 
     public void addQuestion(QuizQuestion question) {
         // your code goes here
+        questions.add(question);
     }
 
     public QuizQuestion getQuestion(int questionNumber) throws QuestionNotFoundException {
        // your code goes here
+        if (questionNumber <= 0 || questionNumber > questions.size()) {
+            throw new QuestionNotFoundException("Question number " + questionNumber + " does not exist.");
+        }
         return questions.get(questionNumber - 1);
     }
 }
